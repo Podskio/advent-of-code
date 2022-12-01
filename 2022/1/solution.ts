00e84e520e54d006659cf5b77cdbe8fce7c90598
Podskio <podskio1@gmail.com>
2022-12-01 11:39:32 -0800

    Refactor

--- 2022/1/solution.ts
+++ 2022/1/solution.ts
@@ -1,14 +1,17 @@
 const input = await Deno.readTextFile("./input.txt");
 const lines = input.split("\n");
 
-let mostCalories = 0;
+const totalCalories = [];
 let currentElf = 0;
 
 for (const line of lines) {
   if (line == "\r") {
-    if (currentElf > mostCalories) mostCalories = currentElf;
+    totalCalories.push(currentElf);
     currentElf = 0;
   } else currentElf += parseInt(line);
 }
 
-console.log(mostCalories);
+totalCalories.sort((a, b) => b - a);
+
+console.log("Part 1: " + totalCalories[0]);
+console.log("Part 2: " + totalCalories.slice(0, 3).reduce((a, b) => a + b));
