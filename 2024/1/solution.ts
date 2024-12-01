d714b6675c0a3f2082343a9a45758a74d6f54d42
podskio <podskio1@gmail.com>
2024-11-30 21:15:29 -0800

    2024 Day 1

---
+++ 2024/1/solution.ts
@@ -0,0 +1,29 @@
+const input = await Deno.readTextFile("./input.txt");
+const lines = input.split("\n");
+
+const left = [];
+const right = [];
+
+let part1 = 0;
+let part2 = 0;
+
+for (const line of lines) {
+  const [leftNum, rightNum] = line.split(" ").filter(Boolean).map(Number);
+  left.push(leftNum);
+  right.push(rightNum);
+}
+
+left.sort();
+right.sort();
+
+for (let i = 0; i < left.length; i++) {
+  const leftNum = left[i];
+  const rightNum = right[i];
+  const rightMatches = right.filter((n) => n === leftNum);
+
+  part1 += Math.abs(leftNum - rightNum);
+  part2 += leftNum * rightMatches.length;
+}
+
+console.log("Part 1: " + part1);
+console.log("Part 2: " + part2);
