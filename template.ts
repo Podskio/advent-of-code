961cc14e90fe2c61a5431f0d25f689a45a19617e
podskio <podskio1@gmail.com>
2024-12-09 13:43:32 -0800

    Add template

---
+++ template.ts
@@ -0,0 +1,11 @@
+const input = await Deno.readTextFile("./sample.txt");
+const lines = input.split("\n");
+// const grid = lines.map((l) => l.split(""));
+
+let part1 = 0;
+let part2 = 0;
+
+//
+
+console.log("Part 1: " + part1);
+console.log("Part 2: " + part2);
