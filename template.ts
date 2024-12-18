6fd3457ef72b17f54f82caf2a262bbc23fa4052a
podskio <podskio1@gmail.com>
2024-12-18 01:28:15 -0800

    Update template with grid

--- template.ts
+++ template.ts
@@ -1,7 +1,7 @@
 const day = import.meta.filename?.split("/").pop()?.split(".")[0];
 const input = Deno.readTextFileSync(`${day}.txt`);
 const lines = input.split("\n");
-// const grid = lines.map((l) => l.split(""));
+// const grid = Grid.fromString(lines);
 
 let part1 = 0;
 let part2 = 0;
